9ed65a3558627bab537f0bce100417c94a2c06ad
yogimp <yogimeizalputra@gmail.com>
2023-07-25 13:43:38 +0700

    Adding project Files

---
+++ src/App.tsx
@@ -0,0 +1,59 @@
+import Home from "./pages/home/Home"
+import './styles/global.scss'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+} from "react-router-dom";
+import Products from "./pages/products/Products";
+import Users from "./pages/users/Users";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Menu from "./components/menu/Menu";
+
+function App() {
+
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    )
+  }
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />
+        },
+        {
+          path: "/users",
+          element: <Users />
+        },
+        {
+          path: "/products",
+          element: <Products />
+        },
+      ]
+    },
+  ]);
+
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+export default App
