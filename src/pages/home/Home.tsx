f1211e7ce12323760c7a7934db7b0fc0d3f8db4a
yogimp <yogimeizalputra@gmail.com>
2023-07-31 07:56:40 +0700

    Adding BigChart on Content

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,6 +4,7 @@ import ChartBox from '../../components/chartBox/ChartBox'
 import { barChartBoxRevenue, barChartBoxVisit, chartBoxConversion, chartBoxProduct, chartBoxRevenue, chartBoxUser } from '../../data'
 import BarChartBox from '../../components/barChartBox/BarChartBox'
 import PieChartBox from '../../components/pieChartBox/PieChartBox'
+import BigChartBox from '../../components/bigChartBox/BigChartBox'
 
 const Home = () => {
     return (
@@ -26,7 +27,7 @@ const Home = () => {
             <div className="box box6">
                 <ChartBox {...chartBoxRevenue} />
             </div>
-            <div className="box box7">Box7</div>
+            <div className="box box7"><BigChartBox /></div>
             <div className="box box8"><BarChartBox {...barChartBoxVisit} /></div>
             <div className="box box9"><BarChartBox {...barChartBoxRevenue} /></div>
         </div>
